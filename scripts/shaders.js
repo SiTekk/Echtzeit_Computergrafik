88b02da03c6401136daa634598ac3bf19679c984
Simon <43453351+SiTekk@users.noreply.github.com>
2023-10-31 08:57:10 +0100

    Updated fragment shader

--- scripts/shaders.js
+++ scripts/shaders.js
@@ -7,11 +7,13 @@ uniform mat4 view;
 uniform mat4 proj;
 
 out vec3 outColor;
+out mat4 pos;
 
 void main()
 {
     gl_Position = proj * view * model * vec4(aVertexPosition.x, aVertexPosition.y, aVertexPosition.z, 1.0f);
     outColor = aColor;
+    pos = model;
 }
 `;
 
@@ -21,73 +23,29 @@ precision mediump float;
 uniform float u_time;
 
 in vec3 outColor;
-
-const vec2 resolution = vec2(256.0, 256.0);
+in mat4 pos;
 
 out vec4 fragColor;
 
-float random (in vec2 t) {
-    return fract(sin(dot(t.xy, vec2(12.9898,78.233)))*43758.5453123);
-  }
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233)))* 43758.5453123);
+}
 
-  float noise (in vec2 t) {
-    vec2 i = floor(t);
-    vec2 f = fract(t);
+void main()
+{
+    vec2 i = gl_FragCoord.xy;
 
-    // Four corners in 2D of a tile
-    float a = random(i);
-    float b = random(i + vec2(1.0, 0.0));
-    float c = random(i + vec2(0.0, 1.0));
-    float d = random(i + vec2(1.0, 1.0));
+    vec4 vecRand;
 
-    vec2 u = f * f * (3.0 - 2.0 * f);
+    vecRand.x = random(i);
+    vecRand.y = random(i + vec2(1.0, 0.0));
+    vecRand.z = random(i + vec2(0.0, 1.0));
+    vecRand.w = random(i + vec2(1.0, 1.0));
 
-    return mix(a, b, u.x) +
-            (c - a)* u.y * (1.0 - u.x) +
-            (d - b) * u.x * u.y;
-  }
+    vec4 color = vec4(sin(outColor.x + u_time + pos[3][0]), cos(outColor.y + u_time + pos[3][1]), sin(outColor.z + u_time + pos[3][2]), 1.0f);
 
-  float fbm (in vec2 t) {
-    float v = 0.0;
-    float a = 0.5;
-    vec2 shift = vec2(100.0);
-    mat2 rot = mat2(cos(0.5), sin(0.5), -sin(0.5), cos(0.5));
-    for (int i = 0; i < 5; ++i) {
-      v += a * noise(t);
-      t = rot * t * 2.0 + shift;
-      a *= 0.5;
-    }
-    return v;
-  }
-
-  void main() {
-    vec2 st = gl_FragCoord.xy/resolution.xy*3.;
-    vec3 color = vec3(0.0);
-
-    vec2 q = vec2(0.);
-    q.x = fbm( st + 0.00*u_time);
-    q.y = fbm( st + vec2(1.0));
-
-    vec2 r = vec2(0.);
-    r.x = fbm( st + 1.0 * q + vec2(1.7,9.2) + 0.15 * u_time );
-    r.y = fbm( st + 1.0 * q + vec2(8.3,2.8) + 0.126 * u_time);
-
-    float f = fbm(st+r);
-
-    color = mix(vec3(0.101961,0.619608,0.666667),
-                vec3(0.666667,0.666667,0.498039),
-                clamp((f*f)*4.0, 0.0, 1.0));
-
-    color = mix(color,
-                vec3(0,0,0.164706),
-                clamp(length(q), 0.0, 1.0));
-
-    color = mix(color,
-                vec3(0.666667,1,1),
-                clamp(length(r.x), 0.0, 1.0));
-
-    fragColor = vec4((f*f*f+.6*f*f+.5*f)*color,1.);
-  }
+    fragColor = mix(color, vecRand, 0.5f);
+}
 `;
 
 function createShaderProgram(gl) {
